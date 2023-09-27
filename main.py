098d58195665eecf6bdabdc51c7ce112695d03ec
shengliang.deng <shengliang.deng@tusen.ai>
2023-09-27 18:10:54 +0800

    add zhipuai

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import select
 import time
 import numpy as np
 import openai
+import zhipuai
 from typing import Tuple, List
 
 cfg_name = sys.argv[1]
@@ -40,7 +41,8 @@ def cos_sim(a, b):
 
 class LanguageModelOpenAI:
     def __init__(self, **kwargs):
-        pass
+        openai.api_base = os.environ.get("OPENAI_API_BASE", "https://api.openai.com/v1")
+        openai.api_key = os.environ["OPENAI_API_KEY"]
 
     def generate(self, prompt):
         while True:
@@ -69,6 +71,37 @@ class LanguageModelOpenAI:
                 time.sleep(10)
             except openai.error.APIError:
                 logger.warning('API error, will try again in 10s')
+                time.sleep(10)
+        return embedding
+
+
+class LanguageModelZhipu:
+    def __init__(self, **kwargs):
+        zhipuai.api_key = os.environ["ZHIPUAI_API_KEY"]
+
+    def generate(self, prompt):
+        while True:
+            try:
+                completion = zhipuai.model_api.invoke(
+                    model="chatglm_lite",
+                    prompt=[{"role":"user", "content": prompt}]
+                )
+                break
+            except Exception as e:
+                logger.error(str(e))
+                time.sleep(10)
+        return completion
+
+    def encode(self, inp: str):
+        while True:
+            try:
+                embedding = zhipuai.model_api.invoke(
+                    model="text_embedding",
+                    prompt=inp
+                )
+                break
+            except Exception as e:
+                logger.error(str(e))
                 time.sleep(10)
         return embedding
 
@@ -336,9 +369,6 @@ Summarize important information from the chat above into STAND-ALONE pieces in t
         with self.lock:
             self.memory.reflect()
 
-
-openai.api_base = os.environ.get("OPENAI_API_BASE", "https://api.openai.com/v1")
-openai.api_key = os.environ["OPENAI_API_KEY"]
 
 language_model = LanguageModel()
 memory = Memory(
